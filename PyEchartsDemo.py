9b39a6803edfc5db6908bda9337089b3fb351321
zhou <qdone8@126.com>
2020-11-03 16:16:37 +0800

    首次批量提交代码

---
+++ PyEchartsDemo.py
@@ -0,0 +1,35 @@
+from pyecharts import options as opts
+from pyecharts.charts import Bar,Pie
+from pyecharts.faker import Faker
+from pyecharts.globals import ThemeType
+import random
+
+bar=(
+    Bar(init_opts=opts.InitOpts(theme=ThemeType.ROMANTIC))
+    .add_xaxis(['衬衫','羊毛衫','雪纺衫','裤子','高跟鞋','袜子'])
+    .add_yaxis('商家A',[5,30,23,45,25,40])
+    .add_yaxis('商家B',[10,20,40,22,29,50])
+    .add_yaxis('商家C',[100,26,29,89,59,200])
+    .set_global_opts(title_opts=opts.TitleOpts(title='主标题',subtitle='副标题'))
+)
+bar.render('./echarts/render.html')
+
+
+bar1=(
+    Bar(init_opts=opts.InitOpts(theme=ThemeType.ESSOS))
+    .add_xaxis(['衬衫','羊毛衫','雪纺衫','裤子','高跟鞋','袜子'])
+    .add_yaxis('商家D',[random.randint(10,100) for _ in range(6)])
+    .add_yaxis('商家E',[random.randint(10,100) for _ in range(6)])
+    .add_yaxis('商家F',[random.randint(10,100) for _ in range(6)])
+    .set_global_opts(title_opts=opts.TitleOpts(title='主标题',subtitle='副标题'))
+)
+bar1.render('./echarts/renderC.html')
+
+
+c = (
+    Pie()
+    .add("", [list(z) for z in zip(Faker.choose(), Faker.values())])
+    .set_global_opts(title_opts=opts.TitleOpts(title="Pie-基本示例"))
+    .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
+    .render("./echarts/pie_base.html")
+)
---
+++ main.py
@@ -0,0 +1,90 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+
+import sys, time, os
+
+
+class WebCharts(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("PyQt5 Web Charts")
+        self.setGeometry(10, 40, 940, 570)
+        self.base = r'file:///d:/charts/echarts/'  # 默认的路径
+        self.url = r""  # 默认render.html
+        self.combo = QComboBox(self)
+        self.combo.addItems(self.getHtmlFile())
+        self.combo.currentIndexChanged.connect(self.changeUrl)
+        self.url = self.combo.currentText()
+
+        self.browser = QWebEngineView(self)
+        self.browser.setGeometry(0, 0, self.width(), self.height() - 50)
+
+        self.browser.load(QUrl(self.base + self.url))
+
+        self.btn = QPushButton("手动刷新网页")
+        self.btn.clicked.connect(lambda: self.loadweb(self.base + self.url))
+
+        self.btn_auto = QPushButton('自动刷新网页')
+        self.btn_auto.clicked.connect(self.autoload)
+
+        self.timer = QTimer(self)  # 5秒刷新计时器
+        self.timer.timeout.connect(lambda: self.loadweb(self.base + self.url))
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.combo)
+        hbox.addWidget(self.btn)
+        hbox.addWidget(self.btn_auto)
+
+        hboxweb = QHBoxLayout()
+        hboxweb.addWidget(self.browser)
+
+        vbox = QVBoxLayout()
+        vbox.addLayout(hboxweb)
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+
+    def loadweb(self, url):
+        '''载入传入的本地html文件的url'''
+
+        #调用getHtmlFile方法，更新返回echarts文件夹中的html文件名的列表
+        self.getHtmlFile()
+        #启动窗口时自动载入下拉框中的当前html文件
+        self.browser.load(QUrl(url))
+
+    def changeUrl(self):
+        '''改变下拉框中的html文件名称'''
+        # 设置类属性self.url的值为下拉框的当前值
+        self.url = self.combo.currentText()
+        # 发射按钮的clicked信号
+        self.btn.clicked.emit()
+
+    # todo:实现按钮的停止刷新文字倒计时5秒
+    def autoload(self):
+        '''每隔5秒 ，自动更新当前页面的数据'''
+
+        #获取当前信号发送者
+        sender = self.sender()
+        if sender.text() == '自动刷新网页':
+            self.btn_auto.setText('停止刷新')
+            self.timer.start(5000)
+        elif sender.text() == '停止刷新':
+            self.timer.stop()
+            self.btn_auto.setText('自动刷新网页')
+
+    def getHtmlFile(self):
+        '''获取当前echarts文件夹中的所有html文件名'''
+        fileList = os.listdir('./echarts/')
+        return fileList  #返回获得的当前文件夹中的文件名列表
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    demo = WebCharts()
+    demo.show()
+    sys.exit(app.exec_())
