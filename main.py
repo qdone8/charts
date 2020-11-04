dfd6cfdeaa6b30b4a6569f4059e6fdc9f512f97d
zhou <qdone8@126.com>
2020-11-04 10:19:40 +0800

    清除reloadData函数的 #TODO标记

--- main.py
+++ main.py
@@ -37,9 +37,8 @@ class WebCharts(QWidget):
 
         self.timer = QTimer(self)  # 根据self.refresh设置的秒数刷新计时器
         self.timer.timeout.connect(lambda: self.loadweb(self.base + self.url))
-        self.btn_data=QPushButton('刷新数据')
+        self.btn_data = QPushButton('刷新数据')
         self.btn_data.clicked.connect(self.reloadData)
-
 
         hbox = QHBoxLayout()
         hbox.addWidget(self.combo)
@@ -86,17 +85,17 @@ class WebCharts(QWidget):
 
     def reloadData(self):
         '''手动刷新下拉列表中的文件名'''
-        # TODO：需要完善增加和删除文件后，索引没有的问题
-        file=self.getHtmlFile()  #获取./echarts目录下的所有html文件名列表
-        currentText=self.combo.currentText() #用currentText记录原下拉框的当前项文本值
+        file = self.getHtmlFile()  # 获取./echarts目录下的所有html文件名列表
+        currentText = self.combo.currentText()  # 用currentText记录原下拉框的当前项文本值
 
         self.combo.clear()
         self.combo.addItems(file)
 
-        c_index=self.combo.findText(currentText) #c_index 当能查到加载前的下拉项文本，ok ；若不能查询到原先相同文件名则设置c_index为默认0值，及显示新内容的首条记录
-        if c_index==-1:
-            c_index=0
+        c_index = self.combo.findText(currentText)  # c_index 当能查到加载前的下拉项文本，ok ；若不能查询到原先相同文件名则设置c_index为默认0值，及显示新内容的首条记录
+        if c_index == -1:
+            c_index = 0
         self.combo.setCurrentIndex(c_index)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
